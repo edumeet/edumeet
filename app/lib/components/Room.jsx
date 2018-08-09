3a0d6f3622e1906f1e9a3a1dc7cfcdff7a80a42e
Torjus <torjusti@gmail.com>
2018-08-09 10:19:53 +0200

    Revert "Attempt fixing scrolling by preventing touchmove events"
    This reverts commit bf4a2e489f3e80ec7037c01a3e413bd5de301f74.

--- app/lib/components/Room.jsx
+++ app/lib/components/Room.jsx
@@ -49,24 +49,16 @@ class Room extends React.Component
 		this.waitForHide();
 	}
 
-	handleTouchMove = (event) =>
-	{
-		event.preventDefault();
-		event.stopPropagation();
-	};
-
 	componentDidMount()
 	{
 		window.addEventListener('mousemove', this.handleMovement);
 		window.addEventListener('touchstart', this.handleMovement);
-		document.body.addEventListener('touchmove', this.handleTouchMove, false);
 	}
 
 	componentWillUnmount()
 	{
 		window.removeEventListener('mousemove', this.handleMovement);
 		window.removeEventListener('touchstart', this.handleMovement);
-		document.body.removeEventListener('touchmove', this.handleTouchMove);
 	}
 
 	render()
