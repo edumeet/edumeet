d3d3e9626efa28ff55808c98ecd9b45d77bf9842
Håvar Aambø Fosstveit <havar@fosstveit.net>
2019-11-04 20:37:25 +0100

    Remove event emits. Setter handles emitting on change.

--- server/lib/Peer.js
+++ server/lib/Peer.js
@@ -106,12 +106,10 @@ class Peer extends EventEmitter
 			email && (this.email = email);
 
 			this.authenticated = true;
-			this.emit('authenticationChanged');
 		}
 		else
 		{
 			this.authenticated = false;
-			this.emit('authenticationChanged');
 		}
 	}
 
