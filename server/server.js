97fc02a5433bf4a476a7e00953f8f217be4c7ff2
Mészáros Mihály <misi@majd.eu>
2020-03-26 21:21:42 +0100

    Fix https redirect

--- server/server.js
+++ server/server.js
@@ -391,7 +391,7 @@ async function runHttpsServer()
 
 	app.all('*', async (req, res, next) =>
 	{
-		if (req.secure)
+		if (req.secure || config.httpOnly )
 		{
 			const ltiURL = new URL(`${req.protocol }://${ req.get('host') }${req.originalUrl}`);
 
