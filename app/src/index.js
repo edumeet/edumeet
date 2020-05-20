a1aec0a8d36bd0558ce32834fb99393db9a4d65e
Håvar Aambø Fosstveit <havar@fosstveit.net>
2020-05-20 23:44:11 +0200

    Cleanups

--- app/src/index.js
+++ app/src/index.js
@@ -153,39 +153,42 @@ function run()
 	// Get current device.
 	const device = deviceInfo();
 
-	let unsupportedBrowser=false;
+	let unsupportedBrowser = false;
 
-	let webrtcUnavailable=false;
+	let webrtcUnavailable = false;
 
 	if (detectDevice() === undefined)
 	{
-		logger.error('Unsupported browser detected by mediasoup client detectDevice! deviceInfo: %o', device);
-		unsupportedBrowser=true;
+		logger.error('Your browser is not supported [deviceInfo:"%o"]', device);
+
+		unsupportedBrowser = true;
 	}
-	else
-	if (
+	else if (
 		navigator.mediaDevices === undefined ||
 		navigator.mediaDevices.getUserMedia === undefined ||
 		window.RTCPeerConnection === undefined
 	)
 	{
-		logger.error('WebRTC is unavialable in your browser! deviceInfo: %o', device);
-		webrtcUnavailable=true;
+		logger.error('Your browser is not supported [deviceInfo:"%o"]', device);
+
+		webrtcUnavailable = true;
 	}
-	else
-	if (!device.bowser.satisfies(
-		window.config.supportedBrowsers ? window.config.supportedBrowsers : supportedBrowsers)
+	else if (
+		!device.bowser.satisfies(
+			window.config.supportedBrowsers || supportedBrowsers
+		)
 	)
 	{
 		logger.error(
-			'Your browser is not on the supported list! Ask your server admin to add your browser to the supported list, if you think that your browser should be supported! deviceInfo: %o',
+			'Your browser is not supported [deviceInfo:"%o"]',
 			device
 		);
-		unsupportedBrowser=true;
+
+		unsupportedBrowser = true;
 	}
 	else
 	{
-		logger.debug('Supported Browser! deviceInfo: %o', device);
+		logger.debug('Your browser is supported [deviceInfo:"%o"]', device);
 	}
 
 	if (unsupportedBrowser || webrtcUnavailable)
