7afe252b787c5e3d883c889b091da201ea9bea12
christian2 <christian2@univie.ac.at>
2020-05-30 06:41:48 +0200

    fix docu

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -387,7 +387,7 @@ module.exports =
 	/*
 	prometheus: {
 		deidentify: false, // deidentify IP addresses
-		// listen: 'localhost',
+		// listen: 'localhost', // exporter listens on this address
 		numeric: false, // show numeric IP addresses
 		port: 8889, // allocated port
 		quiet: false // include fewer labels
