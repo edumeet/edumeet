42aa0d20e0dd9d648392071b712aab3cca045efe
N7Remus <remaigabor@gmail.com>
2020-09-21 19:01:29 +0200

    Fix URL sanitizer bug

--- app/src/components/ChooseRoom.js
+++ app/src/components/ChooseRoom.js
@@ -163,7 +163,7 @@ const ChooseRoom = ({
 				<DialogActions>
 					<Button
 						component={Link}
-						to={roomId}
+						to={encodeURI(encodeURI(roomId))}
 						variant='contained'
 						color='secondary'
 					>
