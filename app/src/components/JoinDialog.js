87c6826259945a7c169e18f2c64b7fc28740d59b
Stefan Otto <stefan.otto@uninett.no>
2020-11-02 04:42:08 +0100

    Removed unused 'fragment' from joinDialog

--- app/src/components/JoinDialog.js
+++ app/src/components/JoinDialog.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import { withRoomContext } from '../RoomContext';
