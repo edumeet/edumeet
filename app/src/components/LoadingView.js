2b46121afc9601cd8516caca644da134e61a62df
Roman Drozd <roman@drozd.it>
2020-11-14 22:24:43 +0100

    Compile external styles into component module

--- app/src/components/LoadingView.js
+++ app/src/components/LoadingView.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
 import './LoadingView.css';
 
-const LoadingView = () =>
+const styles = (theme) =>
+	({
+		root :
+		{
+			display              : 'flex',
+			width                : '100%',
+			height               : '100%',
+			backgroundColor      : 'var(--background-color)',
+			backgroundImage      : `url(${window.config ? window.config.background : null})`,
+			backgroundAttachment : 'fixed',
+			backgroundPosition   : 'center',
+			backgroundSize       : 'cover',
+			backgroundRepeat     : 'no-repeat'
+		},
+		loadingView :
+		{
+			height          : '100%',
+			width           : '100%',
+			backgroundColor : 'red'
+		}
+	});
+
+const LoadingView = ({
+	classes
+}) =>
 {
 	return (
-		<div className='LoadingView' />
+		<div className='LoadingView'
+			classes={{
+				paper : classes.loadingView
+			}}
+
+		/>
 	);
 };
 
-export default LoadingView;
+export default withStyles(styles)(LoadingView);
