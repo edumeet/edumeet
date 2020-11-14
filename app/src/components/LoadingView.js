a18e8ef3f3a7c8a9610d9a67a020f4af995505a8
Roman Drozd <roman@drozd.it>
2020-11-14 22:46:50 +0100

    Remove unused styles

--- app/src/components/LoadingView.js
+++ app/src/components/LoadingView.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
-import './LoadingView.css';
-
 const styles = (theme) =>
 	({
 		root :
-		{
-			display              : 'flex',
-			width                : '100%',
-			height               : '100%',
-			backgroundColor      : 'var(--background-color)',
-			backgroundImage      : `url(${window.config ? window.config.background : null})`,
-			backgroundAttachment : 'fixed',
-			backgroundPosition   : 'center',
-			backgroundSize       : 'cover',
-			backgroundRepeat     : 'no-repeat'
-		},
-		loadingView :
 		{
 			height          : '100%',
 			width           : '100%',
@@ -30,12 +16,7 @@ const LoadingView = ({
 }) =>
 {
 	return (
-		<div className='LoadingView'
-			classes={{
-				paper : classes.loadingView
-			}}
-
-		/>
+		<div className={classes.root} />
 	);
 };
 
