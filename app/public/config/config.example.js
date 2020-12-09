3972bec2d6f13bf12d29a7ceba9dbc5a010bf822
Radeox <dawid.weglarz95@gmail.com>
2020-12-09 12:48:30 +0100

    Changed config order by priority

--- app/public/config/config.example.js
+++ app/public/config/config.example.js
@@ -1,9 +1,95 @@
 // eslint-disable-next-line
 var config =
 {
-	loginEnabled    : false,
+	// Port configuration
 	developmentPort : 3443,
 	productionPort  : 443,
+
+	// Show logo if "logo" is not null, else show title
+	logo         : 'images/logo.example.png',
+	title        : 'edumeet',
+
+	// Service & Support URL
+	// if not set then not displayed on the about modals
+	supportUrl   : 'https://support.example.com',
+
+	// Privacy and dataprotection URL or path
+	// (privacy/privacy.html that is a placeholder for your policies)
+	privacyUrl   : 'privacy/privacy.html',
+
+	// Enable login through configured provider
+	loginEnabled : false,
+
+	/**
+	 * Set max number participants in one room that join 
+	 * unmuted. Next participant will join automatically muted
+	 * Default value is 4
+	 * 
+	 * Set it to 0 to auto mute all, 
+	 * Set it to negative (-1) to never automatically auto mute
+	 * but use it with caution
+	 * full mesh audio strongly decrease room capacity! 
+	 */
+	autoMuteThreshold    : 4,
+	background           : 'images/background.jpg',
+
+	// Democratic, Filmstrip
+	defaultLayout        : 'democratic',
+
+	// If true, will show media control buttons in separate
+	// control bar, not in the ME container.
+	buttonControlBar     : false,
+
+	// If false, will push videos away to make room for side
+	// drawer. If true, will overlay side drawer over videos
+	drawerOverlayed      : true,
+
+	// Max number of participant that will be visible as speaker
+	lastN                : 4,
+	mobileLastN          : 1,
+	// Highest number of lastN the user can select manually in UI
+	maxLastN             : 5,
+	// If truthy, users can NOT change number of speakers visible
+	lockLastN            : false,
+
+	// Position of notifications
+	notificationPosition : 'right',
+	// Timeout for autohiding topbar and button control bar
+	hideTimeout          : 3000,
+
+	/**
+	 * Resolutions:
+	 * 
+	 * low ~ 320x240
+	 * medium ~ 640x480
+	 * high ~ 1280x720
+	 * veryhigh ~ 1920x1080
+	 * ultra ~ 3840x2560
+	 * 
+	 * Frame rates: 1, 5, 10, 15, 20, 25, 30
+	 **/
+	defaultResolution             : 'medium',
+	defaultFrameRate              : 15,
+	defaultScreenResolution       : 'veryhigh',
+	defaultScreenSharingFrameRate : 5,
+
+	// The aspect ratio of the video from the camera
+	// this is not changeable in settings, only config
+	videoAspectRatio              : 1.777,
+
+	// The aspect ratio of the videos as shown on
+	// the screen. This is changeable in client settings.
+	// This value must match one of the defined values in
+	// viewAspectRatios EXACTLY (e.g. 1.333)
+	viewAspectRatio  : 1.777,
+	// These are the selectable aspect ratios in the settings
+	viewAspectRatios : [ {
+		value : 1.333, // 4 / 3
+		label : '4 : 3'
+	}, {
+		value : 1.777, // 16 / 9
+		label : '16 : 9'
+	} ],
 
 	/**
 	 * Supported browsers version 
@@ -26,49 +112,16 @@ var config =
 		'samsung internet for android' : '>=11.1.1.52'
 	},
 
-	/**
-	 * Resolutions:
-	 * 
-	 * low ~ 320x240
-	 * medium ~ 640x480
-	 * high ~ 1280x720
-	 * veryhigh ~ 1920x1080
-	 * ultra ~ 3840x2560
-	 * 
-	 **/
+	/* ---------- Advanced settings ---------- */
 
-	/**
-	 * Frame rates:
-	 * 
-	 * 1, 5, 10, 15, 20, 25, 30
-	 * 
-	 **/
-	// The aspect ratio of the videos as shown on
-	// the screen. This is changeable in client settings.
-	// This value must match one of the defined values in
-	// viewAspectRatios EXACTLY (e.g. 1.333)
-	viewAspectRatio  : 1.777,
-	// These are the selectable aspect ratios in the settings
-	viewAspectRatios : [ {
-		value : 1.333, // 4 / 3
-		label : '4 : 3'
-	}, {
-		value : 1.777, // 16 / 9
-		label : '16 : 9'
-	} ],
-	// The aspect ratio of the video from the camera
-	// this is not changeable in settings, only config
-	videoAspectRatio              : 1.777,
-	defaultResolution             : 'medium',
-	defaultFrameRate              : 15,
-	defaultScreenResolution       : 'veryhigh',
-	defaultScreenSharingFrameRate : 5,
 	// Enable or disable simulcast for webcam video
-	simulcast                     : true,
+	simulcast          : true,
+
 	// Enable or disable simulcast for screen sharing video
-	simulcastSharing              : false,
+	simulcastSharing   : false,
+
 	// Simulcast encoding layers and levels
-	simulcastEncodings            :
+	simulcastEncodings :
 	[
 		{ scaleResolutionDownBy: 4 },
 		{ scaleResolutionDownBy: 2 },
@@ -94,6 +147,7 @@ var config =
 		'chrome',
 		'opera'
 	],
+
 	// Socket.io request timeout
 	requestTimeout   : 20000,
 	requestRetries   : 3,
@@ -101,7 +155,8 @@ var config =
 	{
 		tcp : true
 	},
-	// defaults for audio setting on new clients / can be customized and overruled from client side
+
+	// Defaults for audio setting on new clients / can be customized and overruled from client side
 	defaultAudio:
 	{
 		autoGainControl      : false, // default : false
@@ -110,6 +165,7 @@ var config =
 		voiceActivatedUnmute : false, // default : false / Automatically unmute speaking above noisThereshold
 		noiseThreshold       : -60 // default -60 / This is only for voiceActivatedUnmute and audio-indicator
 	},
+
 	// Audio options for now only centrally from config file: 
 	centralAudioOptions:
 	{
@@ -123,51 +179,8 @@ var config =
 		opusPtime           : '20', // default : 20 / minimum packet time (3, 5, 10, 20, 40, 60, 120)
 		opusMaxPlaybackRate : 96000
 	},
-	/**
-	 * Set max number participants in one room that join 
-	 * unmuted. Next participant will join automatically muted
-	 * Default value is 4
-	 * 
-	 * Set it to 0 to auto mute all, 
-	 * Set it to negative (-1) to never automatically auto mute
-	 * but use it with caution
-	 * full mesh audio strongly decrease room capacity! 
-	 */
-	autoMuteThreshold    : 4,
-	background           : 'images/background.jpg',
-	defaultLayout        : 'democratic', // democratic, filmstrip
-	// If true, will show media control buttons in separate
-	// control bar, not in the ME container.
-	buttonControlBar     : false,
-	// If false, will push videos away to make room for side
-	// drawer. If true, will overlay side drawer over videos
-	drawerOverlayed      : true,
-	// Position of notifications
-	notificationPosition : 'right',
-	// Timeout for autohiding topbar and button control bar
-	hideTimeout          : 3000,
-	// max number of participant that will be visible in 
-	// as speaker
-	lastN                : 4,
-	mobileLastN          : 1,
-	// Highest number of lastN the user can select manually in 
-	// userinteface
-	maxLastN             : 5,
-	// If truthy, users can NOT change number of speakers visible
-	lockLastN            : false,
-	// Show logo if "logo" is not null, else show title
-	// Set logo file name using logo.* pattern like "logo.png" to not track it by git 
-	logo                 : 'images/logo.example.png',
-	title                : 'edumeet',
-	// Service & Support URL
-	// if not set then not displayed on the about modals
-	supportUrl           : 'https://support.example.com',
-	// Privacy and dataprotection URL or path
-	// by default privacy/privacy.html
-	// that is a placeholder for your policies
-	//
-	// but an external url could be also used here	 
-	privacyUrl           : 'privacy/privacy.html',
+
+	/* ---------- Theme settings ---------- */
 	theme                :
 	{
 		palette :
