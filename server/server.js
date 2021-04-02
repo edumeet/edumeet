2980bbe5e465e753f06e5e679c5c9df7158f4132
Jiri Mencak <jmencak@users.noreply.github.com>
2021-04-02 19:32:49 +0200

    Set expressSession proxy based on config.httpOnly
    This change allows support for proxy communication with the Node server
    using plain HTTP and using "secure cookies" at the same time.

--- server/server.js
+++ server/server.js
@@ -99,6 +99,7 @@ const session = expressSession({
 	resave            : true,
 	saveUninitialized : true,
 	store             : new RedisStore({ client: redisClient }),
+	proxy             : config.httpOnly === undefined? false: config.httpOnly,
 	cookie            : {
 		secure   : true,
 		httpOnly : true,
