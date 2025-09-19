cb832dea378c93289acf893a4288273a45c97c42
N7Remus <remaigabor@gmail.com>
2025-09-19 14:58:42 +0200

    fix device choosing issue

--- app/src/RoomClient.js
+++ app/src/RoomClient.js
@@ -1506,7 +1506,7 @@ export default class RoomClient
 				const stream = await navigator.mediaDevices.getUserMedia(
 					{
 						audio : {
-							deviceId : { ideal: deviceId },
+							deviceId : { exact: deviceId },
 							sampleRate,
 							channelCount,
 							autoGainControl,
@@ -1713,7 +1713,7 @@ export default class RoomClient
 					{
 						video :
 						{
-							deviceId : { ideal: deviceId },
+							deviceId : { exact: deviceId },
 							...getVideoConstrains(resolution, aspectRatio),
 							frameRate
 						}
@@ -4292,7 +4292,7 @@ export default class RoomClient
 				{
 					video :
 					{
-						deviceId : { ideal: videoDeviceId },
+						deviceId : { exact: videoDeviceId },
 						...getVideoConstrains(resolution, aspectRatio),
 						frameRate
 					}
